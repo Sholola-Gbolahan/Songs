293d9c2aa0dfa45cb2ed2b0cf099bc519fa2f950
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-03-11 12:13:15 +0100

    Added react-redux and react packages to project

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SongList extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Song list</h1>
+            </div>
+            )
+        }
+}
+ 
+export default SongList; 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+import App from './components/App';
+import reducers from './components/reducers';
+
+const root = ReactDom.createRoot(document.querySelector('#root'))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  
+    <React.StrictMode>
+      <Provider store={createStore(reducers)}>
+         <App />
+      </Provider >
+    </React.StrictMode>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
